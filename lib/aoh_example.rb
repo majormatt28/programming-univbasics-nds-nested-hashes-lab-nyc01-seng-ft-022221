68d6ea11e3b3da99d6e3a84c34689bdb26221aa9
Mathew E Newton <elijah2895@hotmail.com>
2021-02-12 21:17:46 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,6 +31,15 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  assembled_aoh = [
+    {:name => "Don Gately", :occupation => "Live-in Staff"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
+    {:name => "Pat Monteseian", :occupation => "Staff"},
+    {:name => "Kate Gompert", :occupation => "None"},
+    {:name => "Bruce Green", :occupation => "Fan of Mildred"}
+  ]
+
+  assembled_aoh[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
